e55bb75eba83db86d72297f6f2588c794c7aceac
Fernando Narbona <fernandonarbona@Fernandos-Mac-mini.local>
2023-09-09 18:14:42 -0400

    assignment error changing let to const

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -3,10 +3,10 @@ import React from "react";
 type ProgressProps = { radius: number; stroke: number; progress: number };
 
 function Progress({ radius, stroke, progress }: ProgressProps) {
-  let normalizedProgress = progress > 100 ? 100 : progress;
-  let normalizedRadius = radius - stroke * 2;
-  let circumference = normalizedRadius * 2 * Math.PI;
-  let strokeDashoffset =
+  const normalizedProgress = progress > 100 ? 100 : progress;
+  const normalizedRadius = radius - stroke * 2;
+  const circumference = normalizedRadius * 2 * Math.PI;
+  const strokeDashoffset =
     circumference - (normalizedProgress / 100) * circumference;
   return (
     <div className="relative flex h-full w-10 items-center justify-center">
