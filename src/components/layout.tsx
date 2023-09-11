000b976ae68cb0557b3cfff40aada5dfc5e8fa99
Fernando Narbona <fernandonarbona@Fernandos-Mac-mini.local>
2023-09-11 01:16:42 -0400

    fixing type issue

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,6 @@
 import { type PropsWithChildren } from "react";
 
-const PageLayout = (props: PropsWithChildren<{}>) => {
+const PageLayout = (props: PropsWithChildren) => {
   return (
     <main className="flex h-screen justify-center">
       <div className="h-full w-full overflow-y-scroll border-x border-slate-500 md:max-w-2xl">
